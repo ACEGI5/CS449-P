9e0f589073df12185fe913bf09e16bec9143ebab
Gabe <gjn96p@umsystem.edu>
2021-02-28 12:48:55 -0600

    added to player

--- NineMansMorris/NineMansMorrisLib/Player.cs
+++ NineMansMorris/NineMansMorrisLib/Player.cs
@@ -2,6 +2,23 @@
 {
     public class Player
     {
+        public char playerColor;
+        public int unplacedPieces;
+        public int piecesInPlay;
+        private bool canFly = false;
+
+        public void flyPieces()
+        {
+            
+        }
+
+        public void isThreePieces()
+        {
+            if (piecesInPlay == 3)
+            {
+                canFly = true;
+            }
+        }
         
     }
 }
