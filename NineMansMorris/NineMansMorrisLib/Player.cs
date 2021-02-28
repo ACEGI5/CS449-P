de06963846ba20af7502b7d8a409c92dc178352a
Gabe <gjn96p@umsystem.edu>
2021-02-28 13:07:22 -0600

    Player Updated

--- NineMansMorris/NineMansMorrisLib/Player.cs
+++ NineMansMorris/NineMansMorrisLib/Player.cs
@@ -4,5 +4,15 @@
     {
         public int piecesToPlace = 9;
         public int piecesInPlay = 0;
+        public bool canFly = false;
+        public bool allPiecesPlaced = false;
+
+        public void threePiecesLeft()
+        {
+            if (piecesInPlay == 3 && allPiecesPlaced)
+            {
+                canFly = true;
+            }
+        }
     }
 }
