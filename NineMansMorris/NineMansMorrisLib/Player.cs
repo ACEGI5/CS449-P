971ebdb26e3ff9ebe7b07409ebb893923bac3677
Gabe <gjn96p@umsystem.edu>
2021-03-01 10:05:57 -0600

    SetPlayerColor and PiecesPlaced methods added

--- NineMansMorris/NineMansMorrisLib/Player.cs
+++ NineMansMorris/NineMansMorrisLib/Player.cs
@@ -2,19 +2,33 @@
 {
     public class Player
     {
-        public int piecesToPlace = 9;
-        public int piecesInPlay = 0;
-        public bool allPiecesPlaced = false;
-        public char playerColor;
+        public int PiecesToPlace = 9;
+        public int PiecesInPlay = 0;
+        public bool AllPiecesPlaced = false;
+        public char PlayerColor;
+
+        public void SetPlayerColor(char color)
+        {
+            PlayerColor = color;
+        }
 
         // Checks if a player has only three pieces left.
         // If so, player may begin flying pieces.
         // Output: bool representing if player can fly.
-        public bool playerCanFly()
+        public bool PlayerCanFly()
         {
-            return (piecesInPlay == 3 && allPiecesPlaced);
+            return (PiecesInPlay == 3 && AllPiecesPlaced);
         }
-        
-        
+
+        // Checks if player has placed all pieces.
+        // If so, player must start moving placed pieces on board.
+        // Output: returns no value.
+        public void PiecesPlaced()
+        {
+            if (PiecesToPlace == 0)
+            {
+                AllPiecesPlaced = true;
+            }
+        }
     }
 }
