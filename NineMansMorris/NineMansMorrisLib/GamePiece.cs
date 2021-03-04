654304dea17bb8f042ca63810c823cf289397182
Edgar <cerna_edgar@yahoo.com>
2021-03-03 23:27:39 -0600

    Added constructor and color to GamePiece, including an enum for color choice.

--- NineMansMorris/NineMansMorrisLib/GamePiece.cs
+++ NineMansMorris/NineMansMorrisLib/GamePiece.cs
@@ -2,6 +2,25 @@
 {
     public class GamePiece
     {
+        public enum PieceColor
+        {
+            black,
+            white,
+            open
+        }
+        private PieceColor color;
+        public GamePiece()
+        {
+            color = PieceColor.open;
+        }
         
+        public PieceColor getColor()
+        {
+            return color;
+        }
+        public void setColor(PieceColor newColor)
+        {
+            color = newColor;
+        }
     }
 }
