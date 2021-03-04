ba1dfe4eb67333edcaba5428a3077a170b3b8ed7
iuma-estabrooks <idehn3@mail.umkc.edu>
2021-03-04 00:27:34 -0600

    Added NineMansMorrisLogic.cs variables and methods that need to be created. Please don't yell at me Caleb.

--- NineMansMorris/NineMansMorrisLib/NineMansMorrisLogic.cs
+++ NineMansMorris/NineMansMorrisLib/NineMansMorrisLogic.cs
@@ -1,7 +1,45 @@
-﻿namespace NineMansMorrisLib
+﻿
+using System;
+
+namespace NineMansMorrisLib
 {
     public class NineMansMorrisLogic
     {
+        private int _turn;
+        private Player _whitePlayer;
+        private Player _blackPlayer;
+        private Board _gameBoard;
+        public NineMansMorrisLogic()
+        {
+            Random rnd = new Random();
+            _turn = rnd.Next(0, 2);
+            _whitePlayer = new Player();
+            _blackPlayer = new Player();
+        }
+        
+        public void MovePiece(Player player, int oldRow, int oldColumn, int newRow, int newColumn)
+        {
+            if (player.PlayerCanFly())
+            {
+                
+            }
+            else
+            {
+                
+            }
+        }
+        public void PlacePiece(Player player)
+        {
+            
+        }
+        public void TakeTurn(Player player)
+        {
+            
+        }
+        
+        
+        
+        
         //gabe reallly do suck
     }
 }
