08e85f640c4345eee4b524aa403adc7e23c5b789
canismajor88 <abstfact87@gmail.com>
2021-03-06 09:46:16 -0600

    Updated PopulateReverseDiagonal

--- NineMansMorris/NineMansMorrisLib/Board.cs
+++ NineMansMorris/NineMansMorrisLib/Board.cs
@@ -5,7 +5,7 @@ namespace NineMansMorrisLib
 {
     public class Board
     {
-        public const  int BoardSize = 7;
+        public const int BoardSize = 7;
         private const int CenterOfBoard = BoardSize / 2;
         public GamePiece[,] GameBoard { get; private set; }
 
@@ -14,15 +14,15 @@ namespace NineMansMorrisLib
             GameBoard = new GamePiece[BoardSize, BoardSize];
             PopulateBoard(GameBoard);
         }
-        
+
         //populates board with open and invalid positions
         private static void PopulateBoard(GamePiece[,] boardArray)
-        {   
+        {
             PopulateDiagonal(boardArray);
             PopulateReverseDiagonal(boardArray);
             PopulateMiddleCells(boardArray);
         }
-        
+
         private static void PopulateMiddleCells(GamePiece[,] boardArray)
         {
             for (var row = 0; row < BoardSize; row++)
@@ -37,14 +37,13 @@ namespace NineMansMorrisLib
                     boardArray[row, col] ??= new GamePiece() {PieceState = PieceState.Invalid};
                 }
             }
+
             boardArray[CenterOfBoard, CenterOfBoard].PieceState = PieceState.Invalid;
         }
 
         private static void PopulateReverseDiagonal(GamePiece[,] boardArray)
         {
-            var row = BoardSize - 1;
-            var col = 0;
-            for (; row >= 0 && col < BoardSize; row--, col++)
+            for (int row = BoardSize - 1, col = 0; row >= 0 && col < BoardSize; row--, col++)
             {
                 boardArray[row, col] = new GamePiece {PieceState = PieceState.Open};
             }
