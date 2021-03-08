c1462dacf1b8e4e0cf7181f25d8bee422690f58d
canismajor88 <abstfact87@gmail.com>
2021-03-08 13:47:25 -0800

    Added TestInvalidPiecePlacementOccupiedSpot

--- NineMansMorris/NineMansMorrisTests/LogicTests.cs
+++ NineMansMorris/NineMansMorrisTests/LogicTests.cs
@@ -17,11 +17,19 @@ namespace NineMansMorrisTests
 
         }
         [TestCase(0, 1, PieceState.White)]
-        public void TestInvalidPiecePlacement(int row, int col, PieceState color)
+        public void TestInvalidPiecePlacementInvalidSpot(int row, int col, PieceState color)
         {
             NineMansMorrisLogic sut = new NineMansMorrisLogic();
             sut.PlacePiece(sut.WhitePlayer,row,col);
             Assert.AreEqual(PieceState.Invalid, sut.GameBoard.GameBoard[row, col].PieceState);
+        }
+        [TestCase(0, 0, PieceState.White)]
+        public void TestInvalidPiecePlacementOccupiedSpot(int row, int col, PieceState color)
+        {
+            NineMansMorrisLogic sut = new NineMansMorrisLogic();
+            sut.PlacePiece(sut.BlackPlayer,row,col);
+            sut.PlacePiece(sut.WhitePlayer,row,col);
+            Assert.AreEqual(PieceState.Black, sut.GameBoard.GameBoard[row, col].PieceState);
         }
        
     }
