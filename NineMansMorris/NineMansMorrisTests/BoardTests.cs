6efd888791a6cfe660541d82a4f5c19fa7854abd
canismajor88 <abstfact87@gmail.com>
2021-03-10 02:29:18 -0800

    Did stuff

--- NineMansMorris/NineMansMorrisTests/BoardTests.cs
+++ NineMansMorris/NineMansMorrisTests/BoardTests.cs
@@ -27,5 +27,47 @@ namespace NineMansMorrisTests
             var sut = new Board();
             Assert.AreEqual(sut.GameBoard[row, col].PieceState, state);
         }
+        [TestCase(0, 0, PieceState.Open)]
+        [TestCase(1, 1, PieceState.Open)]
+        [TestCase(2, 2, PieceState.Open)]
+        [TestCase(3, 3, PieceState.Invalid)]
+        [TestCase(4, 4, PieceState.Open)]
+        [TestCase(5, 5, PieceState.Open)]
+        [TestCase(6, 6, PieceState.Open)]
+        public void TestPopulateDiagonal(int row, int col, PieceState state)
+        {
+            //testing side effect of method
+            var sut = new Board();
+            Assert.AreEqual(sut.GameBoard[row, col].PieceState, state);
+        }
+        [TestCase(0, 6, PieceState.Open)]
+        [TestCase(1, 5, PieceState.Open)]
+        [TestCase(2, 4, PieceState.Open)]
+        [TestCase(3, 3, PieceState.Invalid)]
+        [TestCase(4, 2, PieceState.Open)]
+        [TestCase(5, 1, PieceState.Open)]
+        [TestCase(6, 0, PieceState.Open)]
+        public void TestPopulateReverseDiagonal(int row, int col, PieceState state)
+        {//testing side effect of method
+            var sut = new Board();
+            Assert.AreEqual(sut.GameBoard[row, col].PieceState, state);
+        }
+        [TestCase(0, 3, PieceState.Open)]
+        [TestCase(1, 3, PieceState.Open)]
+        [TestCase(2, 3, PieceState.Open)]
+        [TestCase(3, 3, PieceState.Invalid)]
+        [TestCase(4, 3, PieceState.Open)]
+        [TestCase(5, 3, PieceState.Open)]
+        [TestCase(6, 3, PieceState.Open)]
+        [TestCase(3, 0, PieceState.Open)]
+        [TestCase(3, 1, PieceState.Open)]
+        [TestCase(3, 4, PieceState.Open)]
+        [TestCase(3, 5, PieceState.Open)]
+        [TestCase(3, 6, PieceState.Open)]
+        public void TestPopulateMiddleCells(int row, int col, PieceState state)
+        {
+            var sut = new Board();
+            Assert.AreEqual(sut.GameBoard[row, col].PieceState, state);
+        }
     }
 }
--- NineMansMorris/NineMansMorrisTests/LogicTests.cs
+++ NineMansMorris/NineMansMorrisTests/LogicTests.cs
@@ -10,27 +10,92 @@ namespace NineMansMorrisTests
         public void TestValidPlacePiece(int row, int col, PieceState color)
         {
             NineMansMorrisLogic sut = new NineMansMorrisLogic();
-            
-            sut.PlacePiece(sut.WhitePlayer,row,col);
-            
-            Assert.AreEqual(PieceState.White,sut.GameBoard.GameBoard[row,col].PieceState);
 
+            sut.PlacePiece(sut.WhitePlayer, row, col);
+
+            Assert.AreEqual(PieceState.White, sut.GameBoard.GameBoard[row, col].PieceState);
         }
+
         [TestCase(0, 1, PieceState.White)]
         public void TestInvalidPiecePlacementInvalidSpot(int row, int col, PieceState color)
         {
             NineMansMorrisLogic sut = new NineMansMorrisLogic();
-            sut.PlacePiece(sut.WhitePlayer,row,col);
+            sut.PlacePiece(sut.WhitePlayer, row, col);
             Assert.AreEqual(PieceState.Invalid, sut.GameBoard.GameBoard[row, col].PieceState);
         }
+
         [TestCase(0, 0, PieceState.White)]
         public void TestInvalidPiecePlacementOccupiedSpot(int row, int col, PieceState color)
         {
             NineMansMorrisLogic sut = new NineMansMorrisLogic();
-            sut.PlacePiece(sut.BlackPlayer,row,col);
-            sut.PlacePiece(sut.WhitePlayer,row,col);
+            sut.PlacePiece(sut.BlackPlayer, row, col);
+            sut.PlacePiece(sut.WhitePlayer, row, col);
             Assert.AreEqual(PieceState.Black, sut.GameBoard.GameBoard[row, col].PieceState);
         }
+
+        [TestCase(0, 0)]
+        public void TestTurnSwitchingOnPlacement(int row, int col)
+        {
+            NineMansMorrisLogic sut = new NineMansMorrisLogic();
+            if (sut.Turn == 0)
+            {
+                sut.PlacePiece(sut.WhitePlayer, row, col);
+                Assert.AreEqual(sut.Turn, 1);
+            }
+            else if (sut.Turn == 1)
+            {
+                sut.PlacePiece(sut.BlackPlayer, row, col);
+                Assert.AreEqual(sut.Turn, 0);
+            }
+        }
+        [TestCase(0, 1)]
+        public void TestNotTurnSwitchingOnInvalidSpot(int row, int col)
+        {
+            NineMansMorrisLogic sut = new NineMansMorrisLogic();
+            if (sut.Turn == 0)
+            {
+                sut.PlacePiece(sut.WhitePlayer, row, col);
+                Assert.AreEqual(sut.Turn, 0);
+            }
+            else if (sut.Turn == 1)
+            {
+                sut.PlacePiece(sut.BlackPlayer, row, col);
+                Assert.AreEqual(sut.Turn, 1);
+            }
+        }
+        [TestCase(0, 0)]
+        public void TestNotTurnSwitchingOnOccupiedSpot(int row, int col)
+        {
+            NineMansMorrisLogic sut = new NineMansMorrisLogic();
+            sut.PlacePiece(sut.WhitePlayer, row, col);
+            if (sut.Turn == 0)
+            {
+                sut.PlacePiece(sut.WhitePlayer, row, col);
+                Assert.AreEqual(sut.Turn, 0);
+            }
+            else if (sut.Turn == 1)
+            {
+                sut.PlacePiece(sut.BlackPlayer, row, col);
+                Assert.AreEqual(sut.Turn, 1);
+            }
+        }
+        
+        [TestCase(0, 1)]
+        public void TestRandomPlayerSelected(int row, int col)
+        {
+            NineMansMorrisLogic sut = new NineMansMorrisLogic();
+            switch (sut.Turn)
+            {
+                case 0:
+                    Assert.AreEqual(sut.Turn, 0);
+                    break;
+                case 1:
+                    Assert.AreEqual(sut.Turn, 1);
+                    break;
+            }
+        }
+
        
+        
     }
 }
