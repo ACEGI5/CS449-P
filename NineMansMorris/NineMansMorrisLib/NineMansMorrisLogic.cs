81b2d595ef0b2d697d85fc5de7bb3b80e8c79e43
Gabe <gjn96p@umsystem.edu>
2021-03-25 20:48:06 -0500

    Updated adjacency checker

--- NineMansMorris/NineMansMorrisLib/NineMansMorrisLogic.cs
+++ NineMansMorris/NineMansMorrisLib/NineMansMorrisLogic.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace NineMansMorrisLib
 {
@@ -20,22 +21,22 @@ namespace NineMansMorrisLib
             GameBoard = new Board();
         }
 
-        // 
+        // Needs rewriting with new isAdjacent method
         public void MovePiece(Player player, int newRow, int newCol, int oldRow, int oldCol)
         {
-            if (player == WhitePlayer && GameBoard.GameBoard[newRow, newCol].PieceState == PieceState.Open &&
-                WhitePlayer.AllPiecesPlaced && CheckIfAdjacent(newRow, newCol, oldRow, oldCol))
-            {
-                GameBoard.GameBoard[newRow, newCol].PieceState = PieceState.White;
-                GameBoard.GameBoard[oldRow, oldCol].PieceState = PieceState.Open;
-            }
+            //if (player == WhitePlayer && GameBoard.GameBoard[newRow, newCol].PieceState == PieceState.Open &&
+            //    WhitePlayer.AllPiecesPlaced && CheckIfAdjacent(newRow, newCol, oldRow, oldCol))
+            //{
+            //    GameBoard.GameBoard[newRow, newCol].PieceState = PieceState.White;
+            //    GameBoard.GameBoard[oldRow, oldCol].PieceState = PieceState.Open;
+            //}
 
-            else if (player == BlackPlayer && GameBoard.GameBoard[newRow, newCol].PieceState == PieceState.Open &&
-                     WhitePlayer.AllPiecesPlaced && CheckIfAdjacent(newRow, newCol, oldRow, oldCol))
-            {
-                GameBoard.GameBoard[newRow, newCol].PieceState = PieceState.Black;
-                GameBoard.GameBoard[oldRow, oldCol].PieceState = PieceState.Open;
-            }
+            //else if (player == BlackPlayer && GameBoard.GameBoard[newRow, newCol].PieceState == PieceState.Open &&
+            //         WhitePlayer.AllPiecesPlaced && CheckIfAdjacent(newRow, newCol, oldRow, oldCol))
+            //{
+            //    GameBoard.GameBoard[newRow, newCol].PieceState = PieceState.Black;
+            //    GameBoard.GameBoard[oldRow, oldCol].PieceState = PieceState.Open;
+            //}
         }
 
         // 
@@ -133,9 +134,50 @@ namespace NineMansMorrisLib
 
         // this method was moved from Board.cs
         // this method has changed from public to private
-        private bool CheckIfAdjacent(int newRow, int newCol, int oldRow, int oldCol)
+        private Hashtable CheckIfAdjacent(int newRow, int newCol, int oldRow, int oldCol)
         {
-            return (Math.Abs(newRow - oldRow) + Math.Abs(newCol - oldCol)) == 1;
+            // Board needs accessor to allow this class to get the board size.
+            Hashtable validMoves = new Hashtable();
+            
+            validMoves.Add("left", new Tuple<int, int>(-1, -1));
+            validMoves.Add("right", new Tuple<int, int>(-1, -1));
+            validMoves.Add("up", new Tuple<int, int>(-1, -1));
+            validMoves.Add("down", new Tuple<int, int>(-1, -1));
+
+            int temp;
+            
+            // Checks case where piece isn't on far left and finds valid move on the left
+            if (newRow > 0)
+            {
+                temp = newRow;
+                while (newRow >= 0)
+                {
+                    // if the first valid spot that is encountered is open, then the left position is set and the loop is broken
+                    if (GameBoard.GameBoard[temp, newCol].PieceState == PieceState.Open)
+                    {
+                        validMoves["left"] = new Tuple<int, int>(temp, newCol);
+                        break;
+                    }
+                    // if spot is invalid, then keep traversing the board to find a valid spot
+                    else if (GameBoard.GameBoard[temp, newCol].PieceState == PieceState.Invalid)
+                    {
+                        temp--;
+                    }
+                    // otherwise, the spot is valid and occupied, and thus can't be moved to
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            // Checks case where piece isn't on far right and finds valid move
+            
+            // Checks case where piece isn't at top and finds valid move
+            
+            // Checks case where piece isn't at bottom and finds valid move
+
+            return validMoves;
         }
     }
 }
