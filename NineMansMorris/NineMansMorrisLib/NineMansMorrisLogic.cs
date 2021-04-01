94b53c6aac972e8e574ab48db0d72b2ab22cfb3c
alexarbuckle <ala2q6@mail.umkc.edu>
2021-04-01 18:18:46 -0500

    fine adjustments

--- NineMansMorris/NineMansMorrisLib/NineMansMorrisLogic.cs
+++ NineMansMorris/NineMansMorrisLib/NineMansMorrisLogic.cs
@@ -4,8 +4,10 @@ using System.Collections.Generic;
 
 namespace NineMansMorrisLib
 {
+    
     public class NineMansMorrisLogic
     {
+        
         public int Turn { get; private set; }
         public Player WhitePlayer { get; private set; }
         public Player BlackPlayer { get; private set; }
@@ -22,16 +24,19 @@ namespace NineMansMorrisLib
 
         public NineMansMorrisLogic()
         {
+            
             var rnd = new Random();
             Turn = rnd.Next(0, 2);
             WhitePlayer = new Player();
             BlackPlayer = new Player();
             GameBoard = new Board();
+            
         }
 
         // pre : 
         public bool movePiece(Player player, int rowTo, int colTo, int rowFrom, int colFrom)
         {
+            
             // if valid placement and all pieces placed
             if (isValid(rowTo, colTo, rowFrom, colFrom) && player.AllPiecesPlaced)
             {
@@ -73,10 +78,12 @@ namespace NineMansMorrisLib
             // Checks if it's white players' turn and if the chosen position is open.
             if (player == WhitePlayer && GameBoard.GameBoard[newRow, newCol].PieceState == PieceState.Open)
             {
+                
                 // Places white in the new position and makes the old position open.
                 GameBoard.GameBoard[newRow, newCol].PieceState = PieceState.White;
                 GameBoard.GameBoard[oldRow, oldCol].PieceState = PieceState.Open;
                 Turn = 1;
+                
             }
 
             // Checks if it's white players' turn and if the chosen position is open.
@@ -86,77 +93,103 @@ namespace NineMansMorrisLib
                 GameBoard.GameBoard[newRow, newCol].PieceState = PieceState.Black;
                 GameBoard.GameBoard[oldRow, oldCol].PieceState = PieceState.Open;
                 Turn = 0;
+                
             }
+            
         }
 
         public void PlacePiece(Player player, int row, int col)
         {
+            
             // checks if it is white player's turn and if
             // this condition was adjusted with regards to isAdjacent
             if (player == WhitePlayer && GameBoard.GameBoard[row, col].PieceState == PieceState.Open &&
                 WhitePlayer.AllPiecesPlaced == false)
             {
+                
                 WhitePlayer.PlacePiece();
                 GameBoard.GameBoard[row, col].PieceState = PieceState.White;
                 Turn = 1;
+                
             }
 
             // this condition was adjusted with regards to isAdjacent
             else if (player == BlackPlayer && GameBoard.GameBoard[row, col].PieceState == PieceState.Open &&
                      BlackPlayer.AllPiecesPlaced == false)
             {
+                
                 BlackPlayer.PlacePiece();
                 GameBoard.GameBoard[row, col].PieceState = PieceState.Black;
                 Turn = 0;
+                
             }
+            
         }
 
         // 
         public void TakeTurn(Player player, int newRow, int newCol, int oldRow, int oldCol)
         {
+            
             if (Turn == 0)
             {
+                
                 //WhitePlayer
                 if (WhitePlayer.PiecesToPlace != 0)
                 {
+                    
                     PlacePiece(WhitePlayer, newRow, newCol);
+                    
                 }
 
                 else if (!WhitePlayer.PlayerCanFly())
                 {
+                    
                     MovePiece(WhitePlayer, newRow, newCol, oldRow, oldCol);
+                    
                 }
 
                 else if (WhitePlayer.PlayerCanFly())
                 {
+                    
                     FlyPiece(WhitePlayer, newRow, newCol, oldRow, oldCol);
+                    
                 }
 
                 //check mill function then -> prompt for player to select which opp piece to mill 
                 Turn = 1;
+                
             }
 
             else if (Turn == 1)
             {
+                
                 //Black Player
                 if (WhitePlayer.PiecesToPlace != 0)
                 {
+                    
                     PlacePiece(WhitePlayer, newRow, newCol);
+                    
                 }
 
                 else if (!WhitePlayer.PlayerCanFly())
                 {
+                    
                     MovePiece(WhitePlayer, newRow, newCol, oldRow, oldCol);
+                    
                 }
 
                 else if (WhitePlayer.PlayerCanFly())
                 {
+                    
                     FlyPiece(WhitePlayer, newRow, newCol, oldRow, oldCol);
+                    
                 }
 
                 //check mill function then -> prompt for player to select which opp piece to mill  
                 Turn = 0;
+                
             }
+            
         }
 
         // pre : 
