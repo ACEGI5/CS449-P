361b5495a175b2e7aa1c711062b495ad7b1e778e
Gabe <gjn96p@umsystem.edu>
2021-05-05 16:04:04 -0500

    Finished AutoNineMansMorrisLogicTests Test Cases

--- NineMansMorris/NineMansMorrisTests/AutoNineMansMorrisLogicTests.cs
+++ NineMansMorris/NineMansMorrisTests/AutoNineMansMorrisLogicTests.cs
@@ -92,6 +92,107 @@ namespace NineMansMorrisTests
             Assert.AreEqual(numBefore, numAfter);
             Assert.False(didFly);
         }
+        
+        [Test]
+        public void TestAutoGameOver()
+        {
+            var sut = new AutoNineMansMorrisLogic();
+            for (var i = sut.BlackPlayer.PiecesToPlace; i > 0; i--)
+            {
+                sut.PlacePiece(sut.BlackPlayer);
+            }
+            
+            for (var i = sut.BlackPlayer.PiecesInPlay; i > 3; i--)
+            {
+                sut.RemovePiece(sut.WhitePlayer);
+            }
+
+            sut.RemovePiece(sut.WhitePlayer);
+            Assert.True(sut.GameOver);
+        }
+        
+        [Test]
+        public void TestAutoGameOverPlacePiece()
+        {
+            var sut = new AutoNineMansMorrisLogic();
+            for (var i = sut.BlackPlayer.PiecesToPlace; i > 0; i--)
+            {
+                sut.PlacePiece(sut.BlackPlayer);
+            }
+            
+            for (var i = sut.BlackPlayer.PiecesInPlay; i > 3; i--)
+            {
+                sut.RemovePiece(sut.WhitePlayer);
+            }
+
+            sut.RemovePiece(sut.WhitePlayer);
+            Assert.True(sut.GameOver);
+            sut.PlacePiece(sut.BlackPlayer);
+            var numBefore = LogicHelper.GetPieces(PieceState.Black, sut.GameBoard).Count;
+            sut.PlacePiece(sut.BlackPlayer);
+            var numAfter = LogicHelper.GetPieces(PieceState.Black, sut.GameBoard).Count;
+            Assert.AreEqual(numBefore, numAfter);
+        }
+        
+        [Test]
+        public void TestAutoGameOverMovePiece()
+        {
+            var sut = new AutoNineMansMorrisLogic();
+            for (var i = sut.BlackPlayer.PiecesToPlace; i > 0; i--)
+            {
+                sut.PlacePiece(sut.BlackPlayer);
+            }
+            
+            for (var i = sut.BlackPlayer.PiecesInPlay; i > 3; i--)
+            {
+                sut.RemovePiece(sut.WhitePlayer);
+            }
+
+            sut.RemovePiece(sut.WhitePlayer);
+            Assert.True(sut.GameOver);
+            var isValidMovement = sut.MovePiece(sut.BlackPlayer);
+            Assert.IsFalse(isValidMovement);
+        }
+
+        
+        [Test]
+        public void TestAutoGameOverFlyPiece()
+        {
+            var sut = new AutoNineMansMorrisLogic();
+            for (var i = sut.BlackPlayer.PiecesToPlace; i > 0; i--)
+            {
+                sut.PlacePiece(sut.BlackPlayer);
+            }
+            
+            for (var i = sut.BlackPlayer.PiecesInPlay; i > 3; i--)
+            {
+                sut.RemovePiece(sut.WhitePlayer);
+            }
+
+            sut.RemovePiece(sut.WhitePlayer);
+            Assert.True(sut.GameOver);
+            var isValidFly = sut.FlyPiece(sut.BlackPlayer);
+            Assert.IsFalse(isValidFly);
+        }
+        
+        [Test]
+        public void TestAutoGameOverRemovePiece()
+        {
+            var sut = new AutoNineMansMorrisLogic();
+            for (var i = sut.BlackPlayer.PiecesToPlace; i > 1; i--)
+            {
+                sut.PlacePiece(sut.BlackPlayer);
+            }
+
+            sut.PlacePiece(sut.BlackPlayer);
+            for (var i = sut.BlackPlayer.PiecesInPlay; i > 3; i--)
+            {
+                sut.RemovePiece(sut.WhitePlayer);
+            }
+
+            sut.RemovePiece(sut.WhitePlayer);
+            Assert.True(sut.GameOver);
+        }
     }
 }
     
